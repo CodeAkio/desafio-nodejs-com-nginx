67786f670559dfefd42a6b89243edc42c9634273
CodeAkio <siqueira_2004@hotmail.com>
2023-01-18 23:07:07 -0300

    fix: wrong SQL syntax and keywords

--- node/index.js
+++ node/index.js
@@ -1,53 +1,52 @@
-const express = requite('express');
-// const mysql = requite('mysql');
+const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
 const port = 3000;
 
-// const config = {
-//   host: 'db',
-//   user: 'root',
-//   password: 'root',
-//   database: 'nodedb',
-// };
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb',
+};
 
 const conn = mysql.createConnection(config);
 
-// app.get('/', indexHandler);
-// createDatabase(conn);
+app.get('/', indexHandler);
+createTablePeople(conn);
 
 app.listen(port, () => {
   console.log(`Running server at port ${port}`);
 });
 
-// function createDatabase(conn) {
-//   const createTableQuery = 'USE `nodedb`; CREATE TABLE IF NOT EXISTS `people` (`id` INT AUTO_INCREMENT PRIMARY KEY, `name` VARCHAR (255) NOT NULL';
-//   conn.query(createTableQuery);
-// }
+function createTablePeople(conn) {
+  const createTableQuery = 'CREATE TABLE IF NOT EXISTS `people` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, `name` VARCHAR(255) NOT NULL);';
+  conn.query(createTableQuery);
+}
 
 function indexHandler(req, res) {
-  // const insertUserQuery = `INSERT INTO people(name) VALUES('John Doo');`;
-  // conn.query(insertUserQuery);
+  const insertUserQuery = `INSERT INTO people(name) VALUES('John Doo');`;
+  conn.query(insertUserQuery);
 
   let htmlResponse = '<h1>Full Cycle</h1>';
 
-  // const getAllUsersQuery = `SELECT id, name FROM people`;
+  const getAllUsersQuery = `SELECT id, name FROM people`;
 
-  // conn.query(getAllUsersQuery, (error, people, _fields) => {
-  //   if (error) {
-  //     conn.end();
-  //     console.log('Error on get users from DB!');
-  //     throw error;
-  //   }
+  conn.query(getAllUsersQuery, (error, people, _fields) => {
+    if (error) {
+      console.log('Error on get users from DB!');
+      throw error;
+    }
 
-  //   htmlResponse += '<ul>';
+    htmlResponse += '<ul>';
 
-  //   for (const person of people) {
-  //     table += `<li>#${person.id} - ${person.name}</li>`;
-  //   }
+    for (const person of people) {
+      table += `<li>#${person.id} - ${person.name}</li>`;
+    }
 
-  //   htmlResponse += '</ul>';
-  // });
+    htmlResponse += '</ul>';
+  });
 
   res.send(htmlResponse);
 }
